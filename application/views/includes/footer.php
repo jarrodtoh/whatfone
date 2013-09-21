3ccab8f68b4f319fc1e925fccdff14e04850ca49
Jarrod Toh <jarrod.toh@gmail.com>
2013-09-21 16:58:04 +0800

    Added footer

--- application/views/includes/footer.php
+++ application/views/includes/footer.php
@@ -1,5 +1,12 @@
 </body>
-<footer>
-	<p>WhatFone is a student project by NTU's NLP class 2013/14.</p>
+<footer style="min-height:10%; background:#f5f5f5; padding: 15px;">
+    <div class="container">
+        <iframe src="http://ghbtns.com/github-btn.html?user=jarrodtoh&repo=whatfone&type=watch&count=true"
+                allowtransparency="true" frameborder="0" scrolling="0" width="62" height="20"></iframe>
+        <iframe src="http://ghbtns.com/github-btn.html?user=jarrodtoh&repo=whatfone&type=fork&count=true"
+                allowtransparency="true" frameborder="0" scrolling="0" width="53" height="20"></iframe>
+        <p>WhatFone made by students of NTU NLP.</p>
+        <p>Based on <a href="http://getbootstrap.com/">Twitter Bootstrap</a>. Web fonts from <a href="http://www.google.com/">Google</a>.</p>
+    </div>
 </footer>
 </html>
